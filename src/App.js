cbac7ebcd9c5986792ccdc29da7730ba9f522d43
M A <fireb1001@gmail.com>
2019-11-27 12:01:33 +0200

    adding material-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Demo from "./views/Demo";
 import Home from "./views/Home";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
+    <Box height="100%">
+      <Router>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/demo" component={Demo} />
+        <Footer />
+      </Router>
+    </Box>
   );
 }
 
